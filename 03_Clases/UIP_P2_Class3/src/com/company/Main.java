88b54e3e421964c4d5a662f934832de72888d3ad
Anthony Diaz <anthony.diazg@outlook.com>
2017-09-22 21:23:48 -0500

    Clase 3 - Septiembre 22
    Clase del día.

---
+++ 03_Clases/UIP_P2_Class3/src/com/company/Main.java
@@ -0,0 +1,25 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+        System.out.println("Bienvenido a Java!");
+        //Enteros
+        byte a;
+        short b;
+        int c=27;
+        long d;
+
+        //Decimales
+        float e;
+        double f = 0.0;
+
+        //Caracteres
+        char g= 'x';
+        String i = "x";
+        //BOLEANO
+        boolean vivo = true;
+        vivo = false;
+    }
+}
